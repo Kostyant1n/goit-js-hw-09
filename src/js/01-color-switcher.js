bcda3e654333630fd4780463c7526238849495d7
Smilyk Kostiantyn <bmd.Warher@gmail.com>
2022-01-10 17:45:59 +0200

    First HW

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const btnStartElement = document.querySelector('[data-start]');
+const btnStopElement = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+btnStartElement.addEventListener('click', function () {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStopElement.removeAttribute('disabled');
+  btnStartElement.setAttribute('disabled', true);
+});
+
+btnStopElement.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStartElement.removeAttribute('disabled');
+  btnStopElement.setAttribute('disabled', true);
+});
